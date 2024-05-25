885130ab1843331906ac27200ebf98fafee69c65
sk-guo <163017292+sk-guo@users.noreply.github.com>
2024-05-25 19:44:18 +0800

    Initial Indication Arrow

---
+++ Assets/Scripts/StartGameButton.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnableDisableObjects : MonoBehaviour
+{
+    public GameObject IndicationArrow;
+    public GameObject Background;
+    public GameObject Title;
+    public Button StartGame;
+
+
+    public void StartTheGame()
+    {
+        Background.SetActive(false);
+        Title.SetActive(false);
+        StartGame.gameObject.SetActive(false);
+        StartCoroutine(EnableDisableSequence());
+    }
+
+    IEnumerator EnableDisableSequence()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            // Enable IndicationArrow for 1 second
+            IndicationArrow.SetActive(true);
+            yield return new WaitForSeconds(0.5f);
+            // Disable IndicationArrow for 1 second
+            IndicationArrow.SetActive(false);
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        
+    }
+}
